df27796f62d6771cdf4dbe58cbd92bd5373728e1
KimHakdo <khd5675@gmail.com>
2022-02-04 14:50:18 +0900

    행렬 수정

--- MINESWEEPER/MINESWEEPER/MainWindow.xaml.cs
+++ MINESWEEPER/MINESWEEPER/MainWindow.xaml.cs
@@ -57,46 +57,49 @@ namespace MINESWEEPER
             int sizeX = x * 20;
             int sizeY = y * 20;
 
-            mainWindow.Width = x > 12 ? sizeX + 60 : 300;
-            mainWindow.Height = y > 12 ? sizeY + 120 : 360;
-            grid1.Width = sizeX;
-            grid1.Height = sizeY;
+            mainWindow.Width = y > 12 ? sizeX + 60 : 300;
+            mainWindow.Height = x > 12 ? sizeY + 120 : 360;
+            grid1.Height = sizeX;
+            grid1.Width = sizeY;
             grid1.Margin = new Thickness(0, 0, 0, y <= 12 ? (Convert.ToInt32(mainWindow.Height) - sizeY - 80) / 2 : 20);
 
-            for (int i = 0; i < x; i++)
+            for (int i = 0; i < y; i++)
             {
-                for(int j = 0; j < y; j++)
+                for(int j = 0; j < x; j++)
                 {
 
                     ToggleButton button = new ToggleButton();
-                    button.Name = $"button_{j + i * j + 1}";
+                    button.Name = $"button_{j * x + i + 1}";
                     button.Height = 20;
                     button.Width = 20;
                     button.HorizontalAlignment = HorizontalAlignment.Left;
                     button.VerticalAlignment = VerticalAlignment.Top;
                     button.Margin = new Thickness(button.Width * i, button.Height * j, 0, 0);
                     button.PreviewMouseDown += new MouseButtonEventHandler(GameButton_LeftClick);
-
+                    
                     grid1.Children.Add(button);
                 }
             }
         }
-
 
         private void GameButton_LeftClick(object sender, MouseEventArgs e)
         {
             int a = 0;
             if(e.LeftButton == MouseButtonState.Pressed && e.RightButton == MouseButtonState.Pressed)
             {
-                a = 1;
+                
             }
             else if(e.RightButton == MouseButtonState.Pressed)
             {
-                a = 2;
+                
             }
             else if (e.LeftButton == MouseButtonState.Pressed)
             {
-                a = 3;
+                ToggleButton button = sender as ToggleButton;
+                button.IsEnabled = false;
+                string[] buttonNumber = button.Name.Split('_');
+                int buttonNum = int.Parse(buttonNumber[1]);
+                a = buttonNum;
             }
             mineEA.Text = a.ToString();
         }

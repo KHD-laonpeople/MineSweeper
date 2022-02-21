e6385993d4a405b7ccee87a49863728a9d99bfe3
yoonsung.park <yspak82@gmail.com>
2022-02-22 08:36:51 +0900

    add(test): xUnit 테스트 프로젝트 추가함.

---
+++ Test-Minesweeper/Test_CreateMineLocations.cs
@@ -0,0 +1,185 @@
+using MineKit;
+using System;
+using Xunit;
+
+namespace Test_Minesweeper
+{    
+    public class Test_CreateMineLocations
+    {
+        int GetMineCount(bool[,] mines)
+        {
+            int result = 0;
+            foreach (var item in mines)
+                result = result + (item ? 1 : 0);
+            return result;
+        }
+        [Fact(DisplayName = "1. BoardAndMineZero")]
+        public void BoardAndMineZero()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 0;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(0, 0, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == testMineCount);
+        }
+        [Fact]
+        public void Board0x0Mine1()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 1;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(0, 0, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board0x0Mine10()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 10;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(0, 0, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board0x0Mine1000000()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 100000;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(0, 0, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board1x1MineZero()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 0;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(1, 1, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board1x1Mine1()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 1;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(1, 1, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board1x1Mine10()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 10;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(1, 1, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board10x10Mine0()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 0;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(10, 10, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board10x10Mine1()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 1;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(10, 10, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == testMineCount);
+        }
+        [Fact]
+        public void Board10x10Mine5()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 5;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(10, 10, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == testMineCount);
+        }
+        [Fact]
+        public void Board10x10Mine100()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 100;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(10, 10, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == 0);
+        }
+        [Fact]
+        public void Board1000x1000Mine100()
+        {
+            //Arrange
+            int resultMineCount = 0;
+            int testMineCount = 100;
+
+            //Action
+            var mines = MineManager.CreateMineLocations(100, 100, testMineCount);
+            resultMineCount = GetMineCount(mines);
+
+            //Assert
+            Assert.True(resultMineCount == testMineCount);
+        }
+    }
+}

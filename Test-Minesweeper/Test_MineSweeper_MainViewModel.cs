e68ad4e2453d6caab2cd226036e6a3c872516dbf
yoonsung.park <yspak82@gmail.com>
2022-02-22 09:02:33 +0900

    dev(test): xUnit 테스트 케이스 추가함

---
+++ Test-Minesweeper/Test_MineSweeper_MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MINESWEEPER;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test_Minesweeper
+{
+    public class Test_MineSweeper_MainViewModel
+    {
+
+        [Theory]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(0, 0, 1, 0)]
+        [InlineData(0, 0, 10, 0)]
+        [InlineData(10, 10, 10, 100)]
+        public void CreateMineMap(int width, int height, int mineCount, int assertMineCount)
+        {
+            //Arrange
+            var vm = new MainViewModel();
+
+            //Action
+            vm.Width = width;
+            vm.Height = height;
+            vm.MineCount = mineCount;
+            vm.StartStopCommand.Execute().Subscribe();            
+
+            //Assert
+            
+            Assert.True(vm.Mines.Count() == assertMineCount);
+        }
+    }
+}
